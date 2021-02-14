e86b8646a3cead75c45f153263761e39e15a47b9
JaniceZhou <janicezhou828@gmail.com>
2021-02-14 03:26:42 -0500

    changed RL agent action space to 6

--- slimevolleygym/slimevolley.py
+++ slimevolleygym/slimevolley.py
@@ -470,6 +470,133 @@ class Agent:
 
     return canvas
 
+class RL_Agent:
+  """ keeps track of the agent in the game. note this is not the policy network """
+  def __init__(self, dir, x, y, c):
+    self.dir = dir # -1 means left, 1 means right player for symmetry.
+    self.x = x
+    self.y = y
+    self.r = 1.5
+    self.c = c
+    self.vx = 0
+    self.vy = 0
+    self.desired_vx = 0
+    self.desired_vy = 0
+    self.state = RelativeState()
+    self.emotion = "happy"; # hehe...
+    self.life = MAXLIVES
+  def lives(self):
+    return self.life
+  def setAction(self, action):
+    forward = False
+    backward = False
+    jump = False
+
+      action_table = [[0, 0, 0], # 0 NOOP
+                    [1, 0, 0], # 1 LEFT (forward)
+                    [1, 0, 1], # 2 UPLEFT (forward jump)
+                    [0, 0, 1], # 3 UP (jump)
+                    [0, 1, 1], # 4 UPRIGHT (backward jump)
+                    [0, 1, 0]] # 5 RIGHT (backward)
+
+    if action == 1 or action == 2:
+      forward = True
+    if action == 4 or action == 5:
+      backward = True
+    if action ==2 or action == 3 or action == 4:
+      jump = True
+    self.desired_vx = 0
+    self.desired_vy = 0
+    if (forward and (not backward)):
+      self.desired_vx = -PLAYER_SPEED_X
+    if (backward and (not forward)):
+      self.desired_vx = PLAYER_SPEED_X
+    if jump:
+      self.desired_vy = PLAYER_SPEED_Y
+  def move(self):
+    self.x += self.vx * TIMESTEP
+    self.y += self.vy * TIMESTEP
+  def step(self):
+    self.x += self.vx * TIMESTEP
+    self.y += self.vy * TIMESTEP
+  def update(self):
+    self.vy += GRAVITY * TIMESTEP
+
+    if (self.y <= REF_U + NUDGE*TIMESTEP):
+      self.vy = self.desired_vy
+
+    self.vx = self.desired_vx*self.dir
+
+    self.move()
+
+    if (self.y <= REF_U):
+      self.y = REF_U;
+      self.vy = 0;
+
+    # stay in their own half:
+    if (self.x*self.dir <= (REF_WALL_WIDTH/2+self.r) ):
+      self.vx = 0;
+      self.x = self.dir*(REF_WALL_WIDTH/2+self.r)
+
+    if (self.x*self.dir >= (REF_W/2-self.r) ):
+      self.vx = 0;
+      self.x = self.dir*(REF_W/2-self.r)
+  def updateState(self, ball, opponent):
+    """ normalized to side, appears different for each agent's perspective"""
+    # agent's self
+    self.state.x = self.x*self.dir
+    self.state.y = self.y
+    self.state.vx = self.vx*self.dir
+    self.state.vy = self.vy
+    # ball
+    self.state.bx = ball.x*self.dir
+    self.state.by = ball.y
+    self.state.bvx = ball.vx*self.dir
+    self.state.bvy = ball.vy
+    # opponent
+    self.state.ox = opponent.x*(-self.dir)
+    self.state.oy = opponent.y
+    self.state.ovx = opponent.vx*(-self.dir)
+    self.state.ovy = opponent.vy
+  def getObservation(self):
+    return self.state.getObservation()
+
+  def display(self, canvas, bx, by):
+    x = self.x
+    y = self.y
+    r = self.r
+
+    angle = math.pi * 60 / 180
+    if self.dir == 1:
+      angle = math.pi * 120 / 180
+    eyeX = 0
+    eyeY = 0
+
+    canvas = half_circle(canvas, toX(x), toY(y), toP(r), color=self.c)
+
+    # track ball with eyes (replace with observed info later):
+    c = math.cos(angle)
+    s = math.sin(angle)
+    ballX = bx-(x+(0.6)*r*c);
+    ballY = by-(y+(0.6)*r*s);
+
+    if (self.emotion == "sad"):
+      ballX = -self.dir
+      ballY = -3
+
+    dist = math.sqrt(ballX*ballX+ballY*ballY)
+    eyeX = ballX/dist
+    eyeY = ballY/dist
+
+    canvas = circle(canvas, toX(x+(0.6)*r*c), toY(y+(0.6)*r*s), toP(r)*0.3, color=(255, 255, 255))
+    canvas = circle(canvas, toX(x+(0.6)*r*c+eyeX*0.15*r), toY(y+(0.6)*r*s+eyeY*0.15*r), toP(r)*0.1, color=(0, 0, 0))
+
+    # draw coins (lives) left
+    for i in range(1, self.life):
+      canvas = circle(canvas, toX(self.dir*(REF_W/2+0.5-i*2.)), WINDOW_HEIGHT-toY(1.5), toP(0.5), color=COIN_COLOR)
+
+    return canvas
+
 class BaselinePolicy:
   """ Tiny RNN policy with only 120 parameters of otoro.net/slimevolley agent """
   def __init__(self):
@@ -552,7 +679,7 @@ class Game:
     ball_vy = self.np_random.uniform(low=10, high=25)
     self.ball = Particle(0, REF_W/4, ball_vx, ball_vy, 0.5, c=BALL_COLOR);
     self.agent_left = Agent(-1, -REF_W/4, 1.5, c=AGENT_LEFT_COLOR)
-    self.agent_right = Agent(1, REF_W/4, 1.5, c=AGENT_RIGHT_COLOR)
+    self.agent_right = RL_Agent(1, REF_W/4, 1.5, c=AGENT_RIGHT_COLOR)
     self.agent_left.updateState(self.ball, self.agent_right)
     self.agent_right.updateState(self.ball, self.agent_left)
     self.delayScreen = DelayScreen()
@@ -682,12 +809,12 @@ class SlimeVolleyEnv(gym.Env):
     3, # RIGHT
   }
 
-  action_table = [[0, 0, 0], # NOOP
-                  [1, 0, 0], # LEFT (forward)
-                  [1, 0, 1], # UPLEFT (forward jump)
-                  [0, 0, 1], # UP (jump)
-                  [0, 1, 1], # UPRIGHT (backward jump)
-                  [0, 1, 0]] # RIGHT (backward)
+  action_table = [[0, 0, 0], # 0 NOOP
+                  [1, 0, 0], # 1 LEFT (forward)
+                  [1, 0, 1], # 2 UPLEFT (forward jump)
+                  [0, 0, 1], # 3 UP (jump)
+                  [0, 1, 1], # 4 UPRIGHT (backward jump)
+                  [0, 1, 0]] # 5 RIGHT (backward)
 
   from_pixels = False
   atari_mode = False
@@ -720,7 +847,7 @@ class SlimeVolleyEnv(gym.Env):
     if self.atari_mode:
       self.action_space = spaces.Discrete(6)
     else:
-      self.action_space = spaces.MultiBinary(3)
+      self.action_space = spaces.Discrete(6)
 
     if self.from_pixels:
       setPixelObsMode()
